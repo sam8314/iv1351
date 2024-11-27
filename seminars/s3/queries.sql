7d3399ab0f0fdbb00276d8abee56b4bd316ed236
dnnrn <noren.danni@gmail.com>
2024-11-27 17:45:36 +0100

    Update queries.sql

--- seminars/s3/queries.sql
+++ seminars/s3/queries.sql
@@ -22,8 +22,7 @@ ORDER BY
 ----------------------------------------------------------------------------------------
 -- NUMBER OF STUDENTS THERE ARE WITH NO SIBLING, WITH ONE SIBLING AND TWO SIBLINGS
 ----------------------------------------------------------------------------------------
-SELECT 
-    sibling_count AS "No of siblings",
+SELECT  sibling_count AS "No of siblings",
     COUNT(*) AS "No of students"
 FROM (
     SELECT 
@@ -41,6 +40,17 @@ ORDER BY sibling_count;
 -- IDS AND NAMES OF ALL INSTRUCTORS WHO HAVE GIVEN MORE THAN A SPECIFIC NUMBER OF LESSONS DURING THE CURRENT MONTH
 ----------------------------------------------------------------------------------------
 
+SELECT i.id instructor_id, p.first_name, p.last_name, 
+       COUNT(l.id) AS number_of_lessons
+FROM lesson l
+JOIN instructor i ON l.instructor_id = i.id
+JOIN person p ON i.person_id = p.id
+WHERE l.given = true
+       AND EXTRACT(YEAR FROM l.scheduled_time) = EXTRACT(YEAR FROM CURRENT_DATE)
+       AND EXTRACT(MONTH FROM l.scheduled_time) = EXTRACT(MONTH FROM CURRENT_DATE)
+GROUP BY i.id, p.first_name, p.last_name
+HAVING COUNT(l.id) >= 5
+ORDER BY number_of_lessons DESC;
 
 ----------------------------------------------------------------------------------------
 -- LIST OF ALL ENSEMBLES HELD DURING THE NEXT WEEK
